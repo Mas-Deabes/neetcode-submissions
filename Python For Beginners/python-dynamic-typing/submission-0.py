94424cccfe424f3646a33a87a01d43937308e2ef
Sif El Din Deabes <62103437+Mas-Deabes@users.noreply.github.com>
2026-09-10 12:24:59 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Yes"
+print(type(variable))
+variable = ["yes","no","maybe"]
+print(type(variable))
